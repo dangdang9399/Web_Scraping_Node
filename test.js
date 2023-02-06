83222beb19b2b71f1a40c9c1b826a72ac8ec65fa
kimdaeun <07de19@gmail.com>
2023-02-06 22:59:00 +0900

    querySelectorAll test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 
 const scraper = async () => {
 
-    const url = "https://cafe.naver.com/nds07";
+    const url = "https://cafe.naver.com/nds07?iframe_url=/ArticleList.nhn%3Fsearch.clubid=12928625%26search.menuid=994%26search.boardtype=L";
     const browser = await puppeteer.launch({
         headless : false,
         defaultViewport:{
@@ -17,6 +17,7 @@ const scraper = async () => {
     // 사용 시 실제 아이디로 변경하기
     const naver_id = "testtest";
     const naver_pw = "testtesttesttest";
+    // dotenv 라이브러리 사용해서 변경해주기
 
     await page.goto('https://nid.naver.com/nidlogin.login');
 
@@ -28,20 +29,26 @@ const scraper = async () => {
 
     await page.click('.btn_login');
     await page.waitForNavigation();
-
     await page.goto(url);
-    await page.click('#menuLink994');
+    // await page.click('#menuLink994');
 
     await page.waitForSelector('#main-area');
+    await page.click('h3.sub-tit-color');
 
     const links = await page.evaluate(async () => {
         const links = []
 
-        const elements = await document.querySelectorAll('.m-tcol-c');
+            console.log("실행1");
+        const elements = await document.querySelectorAll('a.article');
+            console.log("실행2");
+            console.log("elements === ", elements);
+        // const elements = await document.querySelectorAll('.m-tcol-c');
         // const elements = await document.querySelectorAll('div.m-tcol-c a.article');
 
         for (let el of elements){
-            const link = await el.href;
+            const link = await el;
+            console.log("실행3");
+            console.log("link == ", link);
 
             links.push(link);
         }
@@ -51,8 +58,8 @@ const scraper = async () => {
 
     console.log({links});
 
-    await page.close();
-    await browser.close();
+    // await page.close();
+    // await browser.close();
 
 };
 
