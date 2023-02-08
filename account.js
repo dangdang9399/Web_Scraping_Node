17d42a00659b46cab5f68754b750e76fbb945a98
kimdaeun <07de19@gmail.com>
2023-02-08 22:26:35 +0900

    id pw 정보 gitignore

--- account.js
+++ account.js
@@ -2,8 +2,8 @@
  * 계정정보
  */
 exports.baedal_sesang = function () {//배달세상 (https://cafe.naver.com/nds07)
-    return {id: 'test2', pw: 'test', cafePath:'nds07', clubID:'12928625'};
+    return {id: 'test2', pw: 'test2', cafePath:'nds07', clubID:'12928625'};
 }
 exports.batyumae = function () {//바튜매 (https://cafe.naver.com/bikecargogo)
-    return {id: 'test', pw: 'test'};
+    return {id: 'test2', pw: 'test2'};
 }
