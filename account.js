2dbbfdc3d14d288615d9903278871dc81749b4f1
kimdaeun <07de19@gmail.com>
2023-02-08 22:31:09 +0900

    gitignore setting

--- account.js
+++
@@ -1,9 +0,0 @@
-/**
- * 계정정보
- */
-exports.baedal_sesang = function () {//배달세상 (https://cafe.naver.com/nds07)
-    return {id: 'test2', pw: 'test2', cafePath:'nds07', clubID:'12928625'};
-}
-exports.batyumae = function () {//바튜매 (https://cafe.naver.com/bikecargogo)
-    return {id: 'test2', pw: 'test2'};
-}
