374560d93bf5e839bff8930ec4b0b77ddc7a25f9
kimdaeun <07de19@gmail.com>
2023-02-13 21:35:21 +0900

    게시글 페이징 정보 가져와서 for문 적용중

--- test.js
+++ test.js
@@ -1,49 +1,51 @@
 // google에서 제공하는 웹스크래핑 api
-const puppeteer =require('puppeteer');
+const puppeteer = require('puppeteer');
 
-// 제이쿼리 사용
-const cheerio =require('cheerio');
-
-// 계정 정보 가져오기
-const account =require("../common/account/account");
+// 계정정보
+const account = require('./account');
 
 const scraper = async () => {
 
-    // 배달세상 url 정보
-    const url = "https://cafe.naver.com/nds07?iframe_url=/ArticleSearchList.nhn%3Fsearch.clubid=" + account.baedal_sesang().clubID
-        + "%26search.searchdate=" + account.baedal_sesang().searchDate
-        + "%26search.defaultValue=1%26search.sortBy=date%26userDisplay=" + account.baedal_sesang().userDisplay
+    // 네이버 카페 검색 아이디
+    const url = "https://cafe.naver.com/" + account.naver_cafe().cafePath
+        + "?iframe_url=/ArticleSearchList.nhn%3Fsearch.clubid=" + account.naver_cafe().clubID
+        + "%26search.searchdate=" + account.naver_cafe().searchDate
+        + "%26search.defaultValue=1%26search.sortBy=date%26userDisplay=" + account.naver_cafe().userDisplay
         + "%26search.media=0%26search.option=0";
-
-    // 브라우저 설정
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: {
-            width: 1280,
-            height: 800,
+            width: 2000,
+            height: 1024,
         }
     });
 
     // 탭이 2개 생기는 걸 방지
     const [page] = await browser.pages();
 
+    // 사용 시 실제 아이디로 변경하기
     // 로그인
     await page.goto('https://nid.naver.com/nidlogin.login');
 
+    // 로그인 정보 입력
     await page.evaluate((id, pw) => {
         document.querySelector('#id').value = id;
         document.querySelector('#pw').value = pw;
-    }, account.baedal_sesang().id, account.baedal_sesang().pw);
+    }, account.naver_cafe().id, account.naver_cafe().pw);
     await page.waitForTimeout(1000);
 
     await page.click('.btn_login');
-    await page.waitForNavigation();
 
-    // 로그인 후 카페 페이지 이동
+    // 실제 로그인 되는 id, pw 적고 나서 주석 해제 해주기
+    // await page.waitForNavigation();
+
+    // 로그인 후 네이버 카페로 이동
     await page.goto(url);
 
     // 카페 정보 조회해오기
     await page.waitForSelector('iframe');
+
+    // iframe 정보 사용처리 해주기
     const elementHandle = await page.$(
         'iframe[id="cafe_main"]',
     );
@@ -56,96 +58,52 @@ const scraper = async () => {
 
     // 게시글 리스트 가져오기
     await frame.waitForSelector('.article-board.result-board.m-tcol-c');
-    const linkList = await frame.evaluate(() => {
-        let targetList = [];
-        const list =document.querySelectorAll('.article-board.result-board.m-tcol-c > table > tbody > tr');
-        list.forEach(async (data) => {
-            targetList.push({
-                date : data.querySelector('.td_date').innerText,
-                url : data.querySelector('.td_article .board-list .inner_list a').href,
-                boardNum : data.querySelector('.td_article .board-number .inner_number').innerText
-            })
-        })
-        return targetList
-    });
 
+    // 게시글 페이징 정보 가져오기
     await frame.waitForSelector('#main-area .prev-next');
-    const pageList = await frame.evaluate(()=>{
+    const pageList = await frame.evaluate(() => {
         let pageCountList = [];
-        const list =document.querySelectorAll('#main-area .prev-next');
+        const list = document.querySelectorAll('#main-area .prev-next > a');
         list.forEach(async (data) => {
             pageCountList.push({
-                page : data.$x('//a[contains(@href, \'search.page\')]').innerText
+                pageCountNumber: data.textContent,
+                page: data.href
             })
         })
         return pageCountList
-    })
-    // const pageList = await frame.$x('//*[@id="main-area"]/div[7]/a');
-
+    });
 
     console.log("pageList === ", pageList);
-    // console.log("linkList === ", linkList);
 
+    let boardListPage = await browser.newPage();
+    let boardListPageLinkList = [];
 
+    for (let pageListKey in pageList) {
+        await boardListPage.goto(pageList[pageListKey].page);
 
+        await boardListPage.waitForSelector('iframe');
+        const boardListPageElementHandle = await page.$(
+            'iframe[id="cafe_main"]',
+        );
+        const boardListPageFrame = await boardListPageElementHandle.contentFrame();
 
-
-
-    // 2페이지로 이동
-    // await frame.click('.prev-next a');
-
-
-
-    /// 여기까지 실행됨
-    /*await frame.waitForNavigation();
-
-    const content = await frame.executionContext();
-    const res = await content.evaluate(() => {
-        const el = document.querySelector('div.prev-next');
-        if (el) {
-            console.log(el.outerHTML);
-        }
-    });
-
-    if (res){
-        await elementHandle.evaluate((a, res) => {
-            a.innerHTML = res;
-        }, res);
-    } else {
-        console.log("content 값 없음");
+        // 게시글 리스트 정보 가져오기
+        boardListPageLinkList = await boardListPageFrame.evaluate(() => {
+            let targetList = [];
+            const list = document.querySelectorAll('.article-board.result-board.m-tcol-c > table > tbody > tr');
+            list.forEach(async (data) => {
+                if (data.querySelector('.td_article .board-list .inner_list .list-i-selling .blind').textContent == '판매') {
+                    targetList.push({
+                        date: data.querySelector('.td_date').innerText,
+                        url: data.querySelector('.td_article .board-list .inner_list a').href,
+                        boardNum: data.querySelector('.td_article .board-number .inner_number').innerText
+                    })
+                }
+            })
+            return targetList
+        });
     }
-
-    const result =  await page.evaluate(() => new XMLSerializer().serializeToString(document));*/
-
-
-    // console.log(result ? result : 'null');
-
-    // let pageCnt = await frame.$$('div.prev-next');
-
-    // await frame.select('#searchdate ul.select_list > li', "1d");
-    // await frame.select('#divSearchDate ul.select_list > li"', "1일");
-
-    /*const links = await page.evaluate(async () => {
-        const links = []
-
-        const elements = await document.querySelector('iframe[id="cafe_main"]');
-        elements.content
-        console.log("elements === ", elements);
-        // const elements = await document.querySelectorAll('.m-tcol-c');
-        // const elements = await document.querySelectorAll('div.m-tcol-c a.article');
-
-        for (let el of elements){
-            const link = await el;
-            console.log("실행3");
-            console.log("link == ", link);
-
-            links.push(link);
-        }
-
-        return links;
-    });
-
-    console.log({links});*/
+    console.log("boardListPageLinkList === ", boardListPageLinkList);
 
     await page.waitForTimeout(2000);
     await page.close();
