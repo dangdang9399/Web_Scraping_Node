d609ee1d5b4f09f5a54e4b7e32bb19a3f87226d7
kimdaeun <07de19@gmail.com>
2023-02-20 21:42:22 +0900

    네이버 카페 크롤링 완성

--- test.js
+++ test.js
@@ -78,9 +78,9 @@ const scraper = async () => {
         return pageCountList
     });
 
-    // console.log("pageList === ", pageList);
 
-    // 페이지 수 만큼 for 문 돌면서 게시글 정보 list에 넣어주기
+    let boardListPageLinkList = [];
+    // 페이지 수 만큼 for 문 돌면서 게시글 리스트 정보(게시글 번호, url 등) list에 넣어주기
     for (let pageListKey in pageList) {
         // console.log("pageListKey == ", pageListKey);
         // console.log(pageList[pageListKey].page);
@@ -100,10 +100,11 @@ const scraper = async () => {
             list.forEach(async (data) => {
                 targetList.push({
                     // 테스트용 정보 //
-                    index: ++targetListIndex,
-                    title: data.querySelector('.td_article .board-list .inner_list a').textContent,
+                    // index: ++targetListIndex,
+                    // title: data.querySelector('.td_article .board-list .inner_list a').textContent,
                     // tag: data.querySelector('.td_article .board-list .inner_list .list-i-selling .blind').textContent,
                     // 테스트용 정보 //
+
                     date: data.querySelector('.td_date').innerText,
                     url: data.querySelector('.td_article .board-list .inner_list a').href,
                     boardNum: data.querySelector('.td_article .board-number .inner_number').innerText
@@ -114,39 +115,10 @@ const scraper = async () => {
     }
     // console.log("boardListPageLinkList === ", boardListPageLinkList);
 
-    // 게시글 상세페이지에서 정보 push
+
+    // 게시글 상세페이지에서 정보 추출 후 db 저장
     let pageDetailList = [];
     for (let boardListPageLink of boardListPageLinkList) {
-
-        // 1. 게시글 리스트 수 만큼 새 페이지 생성
-        /*boardListPageLink.forEach(async (link) => {
-            const boardPage = await browser.newPage();
-            await boardPage.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
-            await boardPage.goto(link.url, {waitUntil: 'networkidle0'});
-
-            console.log("실행 1")
-
-            const boardFrameHandle = await boardPage.$("iframe[id='cafe_main']");
-            console.log("실행 2")
-            const boardFramePage = await boardFrameHandle.contentFrame();
-            console.log("실행 3")
-
-            const buyer = await boardFramePage.$eval('.nickname', el => el.innerText);
-            const regDate = await boardFramePage.$eval('.article_info > .date', el => el.innerText);
-            const title = await boardFramePage.$eval('.ProductName', el => el.innerText);
-            const price = await boardFramePage.$eval('.ProductPrice', el => el.innerText);
-
-            console.log("nickname == ", buyer);
-            console.log("regDate == ", regDate);
-            console.log("title == ", title);
-            console.log("price == ", price);
-
-            pageDetailList.push({buyer:buyer, regDate:regDate, title:title, price:price});
-
-            await boardPage.close();
-        });*/
-
-        // 2. 한 페이지에서 페이지 이동
         const boardPage = await browser.newPage();
         await boardPage.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
         for (const link of boardListPageLink) {
@@ -154,14 +126,14 @@ const scraper = async () => {
             const boardFrameHandle = await boardPage.$("iframe[id='cafe_main']");
             const boardFramePage = await boardFrameHandle.contentFrame();
 
-            // 판매 글만 저장
-            // 제목 + 본문에 '판매' 키워드만 들어간 글은 저장 X
+            // 제목 + 본문에 '판매' 키워드만 들어간 판매가 아닌 글은 저장 X
             if (await boardFramePage.$('.ProductName') == null || await boardFramePage.$('.ProductName') == undefined) {
-                console.log("판매 글이 아닙니다.")
+                // console.log("판매 글이 아닙니다.")
                 continue;
 
+                // 판매 글만 저장
             } else if (await boardFramePage.$('.ProductName') !== null || await boardFramePage.$('.ProductName') !== undefined) {
-                console.log("ProductName === ", await boardFramePage.$eval('.ProductName', el => el.innerText));
+                // console.log("ProductName === ", await boardFramePage.$eval('.ProductName', el => el.innerText));
                 // 판매자명
                 const sellerName = await boardFramePage.$eval('.nickname', el => el.innerText);
                 // 등록일
@@ -173,14 +145,21 @@ const scraper = async () => {
                 // 게시글 링크
                 const boardUrl = "https://cafe.naver.com/" + account.baedal_sesang().cafePath + "/" + link.boardNum;
 
-
                 // 본문
                 const content = await boardFramePage.$eval('.se-main-container', el => el.innerText);
 
-                // 판매자 정보 존재하지 않을 때는 본문의 휴대폰 정보만 넣어주기
+
+                /**
+                 * 판매자 정보 존재하지 않을 때는 본문의 휴대폰 정보만 넣어주기
+                 * - ex) 판매 완료
+                 * - ex) 판매자 연락처 정보 존재 X
+                 */
+                // 판매 완료
                 if (await boardFramePage.$('.btn_text') == null || await boardFramePage.$('.btn_text') == undefined) {
-                    console.log("판매자 정보가 없습니다.")
-                    pageDetailList.push({
+                    // console.log("판매자 정보가 없습니다.")
+
+                    // 최종 DB 저장 정보
+                    const dbSetData = {
                         type: 'SESANG',
                         boardID: link.boardNum,
                         sellerName: sellerName,
@@ -189,23 +168,33 @@ const scraper = async () => {
                         price: price,
                         boardUrl: boardUrl,
                         content: content
-                    });
+                    };
+                    pageDetailList.push(dbSetData);
+
+                    // 판매자 정보 없는 경우 DB에 data 저장
+                    // let res = orgNaverCafe.OrgNaverCafeModal(mongoose, dbSetData);
+
+                    // 1~5초 사이 랜덤의 수 생성하고 timeout 설정
+                    const randomNum = (Math.floor(Math.random() * 5) * 1000) + 1000;
+                    await newPromise(resolve => setTimeout(resolve, randomNum));
+
                 } else {
                     await boardFramePage.click('.btn_text');
-                    // 이제 사용하지 않는 코드이므로 newPromise로 변경
-                    // await boardFramePage.waitForTimeout(1000);
+                    // 판매자 정보 불러올 때까지 잠시 대기
                     await newPromise(resolve => setTimeout(resolve, 1000));
 
                     // 판매자 정보
                     let sellerInfoTell;
-
-                    // 판매자 정보는 있지만 전화번호가 없는 경우 판별
                     if (await boardFramePage.$('.tell') == null || await boardFramePage.$('.tell') == undefined) {
+                        // 판매자 연락처 정보 존재 X
                         sellerInfoTell = '';
                     } else {
+                        // 판매자 정보 존재
                         sellerInfoTell = await boardFramePage.$eval('.tell', el => el.innerText);
                     }
-                    pageDetailList.push({
+
+                    // 최종 DB 저장 정보
+                    const dbSetData = {
                         type: 'SESANG',
                         boardID: link.boardNum,
                         sellerName: sellerName,
@@ -215,22 +204,24 @@ const scraper = async () => {
                         boardUrl: boardUrl,
                         sellerPhone: sellerInfoTell,
                         content: content
-                    });
+                    };
+                    pageDetailList.push(dbSetData);
+
+                    // 판매자 정보 있는 경우 DB에 data 저장
+                    // let res = orgNaverCafe.OrgNaverCafeModal(mongoose, dbSetData);
+
+                    // 1~5초 사이 랜덤의 수 생성하고 timeout 설정
+                    const randomNum = (Math.floor(Math.random() * 5) * 1000) + 1000;
+                    await newPromise(resolve => setTimeout(resolve, randomNum));
                 }
-
-                // await boardPage.close();
             }
-
         }
-        console.log("  ");
         await boardPage.close();
-
     }
-    console.log("pageDetailList === ", pageDetailList)
+    // console.log("pageDetailList === ", pageDetailList)
 
     await page.close();
     await browser.close();
-
 };
 
 scraper();
